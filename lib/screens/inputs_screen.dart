207b8b999305b3f5e5c23f92bff19459731ee732
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-05 21:11:13 -0500

    Custom imput field

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class InputsScreen extends StatelessWidget {
   const InputsScreen({super.key});
@@ -13,32 +14,10 @@ class InputsScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('value: $value');
-                },
-                validator: (value) {
-                  if (value == null) return 'Field Required';
-                  return value.length < 4 ? '4 letters min' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  hintText: 'User Name',
-                  labelText: 'Name',
-                  helperText: 'Only Letters',
-                  suffixIcon: Icon(Icons.person_add),
-                  icon: Icon(Icons.assignment_ind),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                    ),
-                  ),
-                ),
+            children: const [
+              CustomInputField(
+                labelText: 'Name',
+                hintText: 'User Name',
               ),
             ],
           ),
